8b196fb4e3d51e18c17091bb5ee2d6e1bcb1dda1
riklaunim <riklaunim@c75b0e88-bd3b-11dd-ab14-3be0eea8eb7e>
2008-12-01 22:01:44 +0000

    basic hachoir and gnome-vfs MIME/META extraction

--- handlers/metafile/handler.py
+++ handlers/metafile/handler.py
@@ -7,6 +7,8 @@ from PyQt4 import QtCore, QtGui, Qsci
 
 from metafileWidget import Ui_MetafileWidget
 from utils import mime
+import hachoir_meta
+import gnome_meta
 
 class metafileWidget(QtGui.QWidget):
 	def __init__(self, parent=None, url=False, mainWindow=False, newTab=False):
@@ -32,6 +34,47 @@ class metafileWidget(QtGui.QWidget):
 		# set the URL
 		self.ui.url.setText(url)
 		self.mainWindow.update_back_bucket(unicode(url))
+		
+		meta = hachoir_meta.get_meta_info(url)
+		if meta and len(meta) > 0:
+			text = '\n<br />'.join(meta)
+			no_meta = False
+		else:
+			mimetype = unicode(mime.get_mime(url))
+			text = '- Mime: %s<br />' % mimetype
+			no_meta = True
+		textWidget = QtGui.QTextBrowser()
+		textWidget.setHtml(text)
+		index = self.ui.metaInfo.currentIndex()
+		self.ui.metaInfo.removeItem(index)
+		self.ui.metaInfo.insertItem(index, textWidget, 'File Info')
+		self.ui.metaInfo.setCurrentIndex(index)
+		
+		meta = gnome_meta.get_meta_info(url)
+		app_name = False
+		if meta and len(meta) > 0:
+			text = u'<b>Mime</b>: %s<br />' % meta['mime']
+			text += u'<b>Description</b>: %s<br />' % meta['description'].decode('utf-8')
+			if meta['default_app'] and len(meta['default_app']) > 0:
+				app_desktop = meta['default_app'][0]
+				app_name = meta['default_app'][1]
+				app_bin = meta['default_app'][2]
+				"""
+				ToDo: execute with application
+				"""
+				text += u'<b>Default application</b>: %s<br />' % app_name
+			if meta['other_apps'] and len(meta['other_apps']) > 0:
+				text += u'<b>Other applications</b>:<br />'
+				for app in meta['other_apps']:
+					if app[1] != app_name:
+						text += u'- %s<br />' % app[1]
+			
+		textWidget = QtGui.QTextBrowser()
+		textWidget.setHtml(text)
+		index  = self.ui.metaInfo.addItem(textWidget, 'GNOME')
+		if no_meta or app_name:
+			self.ui.metaInfo.setCurrentIndex(index)
+			
 		
 		# set the tab
 		if newTab:
