c9b2157d456b175b6cf88cebd5368f4a23fffc09
riklaunim <riklaunim@c75b0e88-bd3b-11dd-ab14-3be0eea8eb7e>
2008-12-02 09:52:42 +0000

    More functionality for httpWidget - reload, stop, page title as tab title etc

--- trunk/handlers/http/handler.py
+++ trunk/handlers/http/handler.py
@@ -3,9 +3,9 @@
 from PyQt4 import QtCore, QtGui
 from httpWidget import Ui_HttpWidget
 
-class webWidget(QtGui.QWidget):
+class httpWidget(QtGui.QWidget):
 	def __init__(self, parent=None, url=False, mainWindow=False, newTab=False):
-		super(webWidget, self).__init__(parent)
+		super(httpWidget, self).__init__(parent)
 		self.ui = Ui_HttpWidget()
 		self.ui.setupUi(self)
 		self.parent = parent
@@ -16,7 +16,7 @@ class webWidget(QtGui.QWidget):
 		# set the url
 		if not url:
 			url = self.ui.url.text()
-
+		
 		self.ui.url.setText(url)
 		self.mainWindow.update_back_bucket(unicode(url))
 		
@@ -42,5 +42,40 @@ class webWidget(QtGui.QWidget):
 		QtCore.QObject.connect(self.ui.home,QtCore.SIGNAL("clicked()"), self.mainWindow.home_clicked)
 		QtCore.QObject.connect(self.ui.url,QtCore.SIGNAL("returnPressed()"), self.mainWindow.url_handler)
 		
+		QtCore.QObject.connect(self.ui.webView,QtCore.SIGNAL("linkClicked (const QUrl&)"), self.link_clicked)
+		QtCore.QObject.connect(self.ui.webView,QtCore.SIGNAL("urlChanged (const QUrl&)"), self.link_clicked)
+		QtCore.QObject.connect(self.ui.webView,QtCore.SIGNAL("loadProgress (int)"), self.load_progress)
+		QtCore.QObject.connect(self.ui.webView,QtCore.SIGNAL("titleChanged (const QString&)"), self.title_changed)
+		QtCore.QObject.connect(self.ui.reload,QtCore.SIGNAL("clicked()"), self.reload_page)
+		
+		
 		QtCore.QMetaObject.connectSlotsByName(self)
+	
+	def title_changed(self, title):
+		"""
+		Web page title changed - change the tab name
+		"""
+		index = self.parent.currentIndex()
+		self.parent.setTabText(index, title)
+	
+	def reload_page(self):
+		"""
+		Reload the web page
+		"""
+		self.ui.webView.setUrl(QtCore.QUrl(self.ui.url.text()))
+	
+	def link_clicked(self, url):
+		"""
+		Update the URL if a link on a web page is clicked
+		"""
+		self.ui.url.setText(url.toString())
+	
+	def load_progress(self, load):
+		"""
+		Page load progress
+		"""
+		if load == 100:
+			self.ui.stop.setEnabled(False)
+		else:
+			self.ui.stop.setEnabled(True)
 	
